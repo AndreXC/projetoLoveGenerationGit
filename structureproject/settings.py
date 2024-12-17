324b10500e93533438006ef042717c0522207a7f
AndreDiasMartins <andre.dias@agilsistemas.inf.br>
2024-12-17 15:31:43 -0300

    alterações da index

--- structureproject/settings.py
+++ structureproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*m5pc1ln#-5zy7z3w_=hywoye7kv5j+-cq@*sc^scmt2ovw)t4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
