c1c2d7723c8cd8b9dd2671198c8bde6b9ba7a55c
AndreXC <andreluismartins2020@outlook.com>
2024-12-31 20:21:28 -0300

    statics Alterações

--- structureproject/settings.py
+++ structureproject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'structureproject', 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Diretório onde o `collectstatic` coloca os arquivos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Diretório onde o `collectstatic` coloca os arquivos
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
