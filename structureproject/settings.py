7596e632da3092d9f231c5188ef2b3589e8212fb
AndreXC <andreluismartins2020@outlook.com>
2025-01-05 16:49:38 -0300

    settingsd

--- structureproject/settings.py
+++ structureproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*m5pc1ln#-5zy7z3w_=hywoye7kv5j+-cq@*sc^scmt2ovw)t4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['lovegeneration.pythonanywhere.com',]
 
